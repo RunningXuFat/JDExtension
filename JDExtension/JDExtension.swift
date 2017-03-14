a5ac2bca3522c1e35754cb6c93b3d7cd1649f0d0
Jaedong <xjae2012@126.com>
2017-03-14 13:35:05 +0800

    Release 1.0.0.

---
+++ JDExtension/JDExtension.swift
@@ -0,0 +1,13 @@
+//
+//  JDExtension.swift
+//  JDExtensionDemo
+//
+//  Created by 徐建东 on 2017/3/14.
+//  Copyright © 2017年 徐建东. All rights reserved.
+//
+
+import UIKit
+
+class JDExtension: NSObject {
+
+}
---
+++ JDExtensionDemo/JDExtensionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JDExtensionDemo
+//
+//  Created by 徐建东 on 2017/3/14.
+//  Copyright © 2017年 徐建东. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
